c7f4961acf0bec9e1a77d1d967f0f375684afb7f
oscar <oscar.urueta.ou@gmail.com>
2021-12-01 11:39:32 -0600

    fix fetch img

--- sw.js
+++ sw.js
@@ -1,27 +1,28 @@
 console.log("hello from sw uwu.js");
 
-// let self = this;
+const contextSW =
+    window.location.href.includes("localhost") ? "/" : "/" + window.location.href.split('/', 4)[3] + "/";
 
-self.addEventListener('install', (event)=>{
+self.addEventListener('install', (event) => {
     console.log("Service worker installed");
 });
 
-self.addEventListener('activate', (event)=>{
+self.addEventListener('activate', (event) => {
     console.log("sw: activated and managing the app");
 });
 
-self.addEventListener('fetch', (event)=>{
+self.addEventListener('fetch', (event) => {
     console.log(event.request.url)
 
     if (event.request.url.includes('save')) {
         let newResponse = new Response("not today.");
-        event.respondWith(newResponse); 
+        event.respondWith(newResponse);
     }
-   
+
     if (event.request.url.includes('.jpg')) {
         console.log(event.request.url);
         console.log("img requested");
-        let newResponse = fetch('/images/steins gate.jpg');
+        let newResponse = fetch(contextSW + 'images/steins gate.jpg');
         event.respondWith(newResponse);
     }
 
@@ -30,10 +31,10 @@ self.addEventListener('fetch', (event)=>{
         background-color:black !important;
         color: red;
     }`, {
-        headers: {
-            'Content-type': 'text/css'
-        }
-    });
-    event.respondWith(newResponse);
+            headers: {
+                'Content-type': 'text/css'
+            }
+        });
+        event.respondWith(newResponse);
     }
 });
